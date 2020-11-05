4fd6c88ddf09b1e5ab1c56a9e49b5c91bf4c0769
shadow <961237327@qq.com>
2020-11-05 21:56:44 +0800

    添加了构造方法

--- src/com/shadow/entity/Student.java
+++ src/com/shadow/entity/Student.java
@@ -4,4 +4,13 @@ public class Student {
     private String id;
     private String name;
     private Integer age;
+
+    public Student() {
+    }
+
+    public Student(String id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
